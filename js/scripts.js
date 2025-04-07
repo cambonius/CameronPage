02ce3f91f0580d423483e3700ef321d0858ad25e
Cameron Sherman <cameronasherman2@gmail.com>
2025-04-06 21:03:17 -0400

    Dark Mode & PNGs

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,26 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the toggle switch element by its id
+    const darkModeToggle = document.getElementById("darkModeToggle");
+  
+    // Check if the user has a saved dark mode preference in localStorage
+    const darkModeEnabled = localStorage.getItem("darkMode") === "true";
+    if (darkModeEnabled) {
+      // If dark mode was enabled, check the toggle and add the dark mode class
+      darkModeToggle.checked = true;
+      document.body.classList.add("dark-mode");
+    }
+  
+    // Listen for changes on the toggle switch
+    darkModeToggle.addEventListener("change", function() {
+      if (this.checked) {
+        // When toggle is switched on, add dark mode class and save preference
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("darkMode", "true");
+      } else {
+        // When toggle is switched off, remove dark mode class and update preference
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("darkMode", "false");
+      }
     });
-
-});
+  });
+  
